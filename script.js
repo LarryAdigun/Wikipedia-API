6b3c1569a8c97073398ff7e9d21b7747070c644c
Larry Adigun <larryadigun.student@careerdevs.com>
2019-04-02 15:27:00 +0000

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-function handleSubmit() {
-    const xhttp = new XMLHttpRequest();
-    const data = {
-        "userName": document.getElementById("wiki").value,
+// function handleSubmit() {
+    
+//     const xhttp = new XMLHttpRequest();
+    
+//     const data =  document.getElementById("wiki").value;
+    
+//     const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + data + "&format=json&callback=?";
+    
+//     document.getElementById("results").innerHTML = this.responseText;
 
-    }
-    xhttp.open("GET", "", true)
-    xhttp.setRequestHeader("Content-type", "application/json")
-    xhttp.send(JSON.stringify(data))
+    
+//     xhttp.open("GET", url, true)
+    
+//     xhttp.setRequestHeader("Content-type", "application/json")
+    
+//     xhttp.send()
+//     console.log(this.responseText)
 
+// }
+
+function handleSubmit(){
+  const xhttp = new XMLHttpRequest();
+  const submitValue = document.getElementById("searchField").value;
+  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + submitValue + "&format=json&callback=?";
+  
+  document.getElementById("results").innerHTML = this.responseText;
+  
+    xhttp.open("GET", url, true);
+    xhttp.send()
+    console.log(this.responseText);
 }
